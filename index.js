235096ef09eb2b49d3e47bc008b225470d50b464
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-04-25 21:30:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-animal = ""
+let animal = "dog"
 
 function myAnimal() {
   return animal
@@ -7,13 +7,15 @@ function myAnimal() {
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
+ let animal = "cat"
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
 
 function add2(n) {
+  let two = 2
   return n + two
 
   // Feel free to move things around!
-  two = 2
+  
 }
